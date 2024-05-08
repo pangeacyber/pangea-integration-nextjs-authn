c2dcdb40233c2140e7fc7597d003c756dbf2a7b4
Pranav Shikarpur <me@snpranav.com>
2024-05-08 21:09:11 +0000

    resolved type issues and fixed grammar on error messages

--- utils/pangeaAuthCheck.ts
+++ utils/pangeaAuthCheck.ts
@@ -1,9 +1,10 @@
 import { PangeaConfig, AuthNService, PangeaErrors } from "pangea-node-sdk";
+import type { NextApiRequest, NextApiResponse } from "next";  
 
 const config = new PangeaConfig({ domain: process.env.PANGEA_DOMAIN });
 const authn = new AuthNService(process.env.PANGEA_TOKEN, config);
 
-const getBearerToken = (req) => {
+const getBearerToken = (req: NextApiRequest) => {
   const authorizationHeader =
   req.headers instanceof Headers
   ? req.headers.get("authorization")
@@ -19,73 +20,79 @@ const getBearerToken = (req) => {
 };
 
 const validateToken = async (token: string) => {
-  const result = false;
+  if(!token) {
+    return false;
+  }
 
-  if (token) {
-    // Check the token against the authn service
-    try {
-      const response = await authn.client.clientToken.check(token);
-      const authStatus = response.status === "Success";
-      return authStatus;
-      
-    } catch (error) {
-      if (error instanceof PangeaErrors.APIError) {
-        console.error("Something went wrong with your Pangea Configuration");
-        console.error(error.toString());
-      } else {
-        console.error(
-          "Error occurred during token validation. Looks like environment variables haven't been set correctly, or the service token has expired",
-          error
-          );
-        }
+  // Check the token against the authn service
+  try {
+    const response = await authn.client.clientToken.check(token);
+    const authStatus = response.status === "Success";
+    return authStatus;
+    
+  } catch (error) {
+    if (error instanceof PangeaErrors.APIError) {
+      console.error("Something went wrong with your Pangea Configuration");
+      console.error(error.toString());
+    } else {
+      console.error(
+        "Error occurred during token validation. Looks like environment variables haven't been set correctly, or the service token has expired",
+        error
+        );
       }
     }
-    return result;
+
+    return false;
 };
   
 // Fetch user Info
-export const getUserInfo = async (req) => {
+export const getUserInfo = async (req: NextApiRequest) => {
   const token = getBearerToken(req);
   const result = {userEmail: "", userProfile: ""};
-  
-  if (token) {
-    // Check the token against the authn service
-    try {
-      const response = await authn.client.clientToken.check(token);
-      const authStatus = response.status === "Success";
-      
-      const userEmail = authStatus ? response.result.email : "";
-      const userProfile = authStatus ? response.result.profile : "";
-      
-      return { userEmail, userProfile };
-      
-    } catch (error) {
-      if (error instanceof PangeaErrors.APIError) {
-        console.error("Something went wrong with your Pangea Configuration");
-        console.error(error.toString());
-      } else {
-        console.error(
-          "Error occured during token validation. Looks like environment variables haven't been set correctly, or the service token has expired",
-          error
-          );
-        }
-      }
+
+  if (!token) {
+    return result;
   }
+  
+  // Check the token against the authn service
+  try {
+    const response = await authn.client.clientToken.check(token);
+    const authStatus = response.status === "Success";
+    
+    const userEmail = authStatus ? response.result.email : "";
+    const userProfile = authStatus ? response.result.profile : "";
+    
+    return { userEmail, userProfile };
+    
+  } catch (error) {
+    if (error instanceof PangeaErrors.APIError) {
+      console.error("Something went wrong with your Pangea Configuration");
+      console.error(error.toString());
+    } else {
+      console.error(
+        "Error occurred during token validation. Looks like environment variables haven't been set correctly, or the service token has expired",
+        error
+      );
+    }
+  }
+
   return result;
 }
     
 
 // Middleware to check the authentication
 // ONLY USE THIS ON SERVER SIDE
-export const withAPIAuthentication = (apiHandler) => {
-  return async (req, res) => {
+export const withAPIAuthentication = <T>(  
+  apiHandler: (req: NextApiRequest, res: NextApiResponse) => Promise<T>,  
+) => {  
+  return async (req: NextApiRequest, res: NextApiResponse) => {
     // Check the environment variables
     if (
       !process.env.PANGEA_DOMAIN || 
       !process.env.PANGEA_TOKEN
       ) {
         console.error(
-          "Missing environment variables, please make sure you have PANGEA_DOMAIN and AUTHN_TOKEN set in your .env file"
+          "Missing environment variables, please make sure you have PANGEA_DOMAIN and PANGEA_TOKEN set in your .env file"
           );
           return res.status(401).json("Unauthorized");
         }
